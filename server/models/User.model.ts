0693bf1eb565328f881c5a63ce7e9db1b7c2ac84
Duc Lux <duc@gmail.com>
2020-05-19 21:47:53 +0700

    [WIP] User routing

---
+++ server/models/User.model.ts
@@ -0,0 +1,48 @@
+import  mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+    fullName: {
+        type: String, 
+        required: true
+    },
+    userType: {
+        type: Number,
+        default: 1002,
+    },
+    phoneNumber: {
+        type: String
+    },
+    email: {
+        type: String, 
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String, 
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+// UserSchema.pre('save', function(next) {
+//     const user = this;
+//     if(!user.isModified('password')) return next()
+//     bscypt
+//         .genSalt(10)
+//         .then(salt => bscypt.hash(user.password, salt))
+//         .then(hash => {
+//             user.password = hash;
+//             return next();
+//         })
+//         .catch(err => {
+//             throw err;
+//         })
+// })
+
+const User = mongoose.model('User', UserSchema, 'users');
+
+export {
+    User,
+    UserSchema
+}

c1245763ed97bd2cc4fe251ec74118946de23175
Nguyen Huu Duc <duc.nh@softworld.jp>
2020-05-20 13:02:11 +0700

    [WIP] User register

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,10 +1,21 @@
-import { Request, Response } from 'express';
-
+import { Request, Response, NextFunction } from 'express';
+import { validatorSignUp, validatorLogin } from '../middlewares/user.validator'
+import { User } from 'models/User.model';
 class UserRoutes {
     public routes(app: any): void {
         app.route('/users')
-            .get((req: Request, res: Response)=> {
-                res.status(200).send("hello")
+            .post( async (req: Request, res: Response, next: NextFunction)=> {
+                const { valid, errors } = validatorSignUp(req.body);
+                if(!valid) res.status(400).json({error: errors});
+
+                const { email, password, fullName, phoneNumber } = req.body;
+                try {
+                    const user = User.findOne({email});
+                    if(user) res.status(400)
+                } catch (error) {
+                    
+                }
+
             } )
     }
 }

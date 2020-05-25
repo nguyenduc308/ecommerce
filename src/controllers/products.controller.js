78d6f25811b35bf3c4a3afecd207acbd9ed5e1a2
Nguyen Huu Duc <duc.nh@softworld.jp>
2020-05-25 12:51:05 +0700

    get all products

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -4,9 +4,12 @@ const slugify = require('slugify');
 
 module.exports.getProducts = async (req, res) => {
     try {
-        
+        const products = await Product.find()
+                                    .populate('categories')
+                                    .populate('variants')
+        return res.status(200).json({products})
     } catch (error) {
-        
+        return res.status(500).json({error: "Server error"})
     }
 }
 

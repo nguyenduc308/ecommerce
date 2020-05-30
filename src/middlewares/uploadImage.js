df5bc7f9f3875494ed8283cfa9c2fb4c847c21ff
duclux <duc@gmail.com>
2020-05-30 17:22:07 +0700

    measure

--- src/middlewares/uploadImage.js
+++ src/middlewares/uploadImage.js
@@ -1,17 +1,17 @@
 const multer = require("multer");
 // const path = require('path');
 
-module.exports.uploadImage = (type, req, res, next) => {
+module.exports.uploadImage = (type) => {
   const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, `./src/uploads/images/${type}s`);
     },
     filename: function (req, file, cb) {
 
-      let math = ["image/png", "image/jpeg", "image/gif"];
-      if (math.indexOf(file.mimetype) === -1) {
+      let mustMatchs = ["image/png", "image/jpeg", "image/gif"];
+      if (mustMatchs.indexOf(file.mimetype) === -1) {
 
-        let errorMess = `The file <strong>${file.originalname}</strong> is invalid. Only allowed to upload image jpeg or png or gif`;
+        let errorMess = `File type does not support !`;
         return cb(errorMess, null);
       }
       let filename = `${Date.now()}-${file.originalname}`;
